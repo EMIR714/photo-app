c5c01b39a6595c7900dc1b7ef7f417d8a03642de
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-02-09 11:46:31 +0600

    fix scaner 3

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   const videoRef = useRef(null);
   const streamRef = useRef(null);
   const canvasRef = useRef(null);
-  const [qrCodeScanner, setQrCodeScanner] = useState(null);
-  const [qrCodeLink, setQrCodeLink] = useState(null);
+  const [isEnabled, setEnabled] = useState(true);
+  const [qrMessage, setQrMessage] = useState("");
+  const [qrCodeScanner, setQrCodeScanner] = useState(null); // Добавлено состояние для qrCodeScanner
 
   const startStream = () => {
     navigator.mediaDevices
@@ -65,25 +66,6 @@ function App() {
     setFacing(facing === "user" ? "environment" : "user");
   };
 
-  const startQRCodeScanner = () => {
-    const scanner = new Html5Qrcode("qr-code-scanner"); // Используйте идентификатор элемента
-    scanner.start(
-      { facingMode: "environment" },
-      {
-        fps: 10,
-        qrbox: { width: 1000, height: 1000 },
-      },
-      (decodedText, decodedResult) => {
-        console.log(`Decoded QR Code: ${decodedText}`);
-        setQrCodeLink(decodedText); // Сохраните ссылку QR-кода
-      },
-      (errorMessage) => {
-        console.error(`QR Code scanning failed: ${errorMessage}`);
-      }
-    );
-    setQrCodeScanner(scanner); // Сохраните экземпляр сканера
-  };
-
   const stopQRCodeScanner = () => {
     if (qrCodeScanner) {
       qrCodeScanner.stop().catch((err) => console.error(err));
@@ -92,18 +74,41 @@ function App() {
 
   useEffect(() => {
     setError(null);
-    stopStream();
-    stopQRCodeScanner(); // Остановите сканер QR-кода
+    stopStream(); 
     startStream();
-    if (facing === "environment") {
-      const startScannerWhenVideoPlays = () => {
-        startQRCodeScanner();
-        videoRef.current.removeEventListener('playing', startScannerWhenVideoPlays);
-      };
-      videoRef.current.addEventListener('playing', startScannerWhenVideoPlays);
-    }
   }, [facing]);
-  
+
+  useEffect(() => {
+    const config = { fps: 10, qrbox: { width: 200, height: 200 } };
+
+    const html5QrCode = new Html5Qrcode("qrCodeContainer");
+
+    const qrScanerStop = () => {
+      if (html5QrCode && html5QrCode.isScanning) {
+        html5QrCode
+          .stop()
+          .then((ignore) => console.log("Scaner stop"))
+          .catch((err) => console.log("Scaner error"));
+      }
+    };
+
+    const qrCodeSuccess = (decodedText) => {
+      setQrMessage(decodedText);
+      setEnabled(false);
+    };
+
+    if (isEnabled) {
+      html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccess);
+      setQrMessage("");
+      setQrCodeScanner(html5QrCode); // Установка qrCodeScanner после запуска сканера
+    } else {
+      qrScanerStop();
+    }
+
+    return () => {
+      qrScanerStop();
+    };
+  }, [isEnabled]);
 
   return (
     <>
@@ -114,15 +119,16 @@ function App() {
         autoPlay
         ref={videoRef}
       ></video>
-      <canvas id="qr-code-scanner" ref={canvasRef}></canvas> {/* Добавьте идентификатор элементу */}
+      <canvas id="qrCodeContainer" ref={canvasRef} ></canvas> 
       {error && <div className="error">{error}</div>}
       <h3>{facing === "user" ? "FRONT CAM" : "BACK CAM"}</h3>
       <div className="controls">
-        <button onClick={() => makePhoto()}>
-          <PhotoIcon />
-        </button>
+        {facing === "user" && <button onClick={() => makePhoto()}><PhotoIcon /></button>}
       </div>
-      {qrCodeLink && <div>Отметка успешно сделана</div>} {/* Отображайте сообщение, когда есть ссылка QR-кода */}
+      {facing === "environment" && <div className="scaner">
+        <div id="qrCodeContainer" />
+        {qrMessage && <div className="qr-message">Вы успешно прошли отметку</div>}
+      </div>}
     </>
   );
 }
