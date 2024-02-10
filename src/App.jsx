8db440d6734603961efd22e13a16bda1e05e4aae
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-02-10 17:23:09 +0600

    fix scaner 9

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,25 @@ function App() {
   const canvasRef = useRef(null);
   const [isEnabled, setEnabled] = useState(true);
   const [qrMessage, setQrMessage] = useState("");
-  const [qrCodeScanner, setQrCodeScanner] = useState(null); // Добавлено состояние для qrCodeScanner
+  const [qrCodeScanner, setQrCodeScanner] = useState(null);
 
   const startStream = () => {
     navigator.mediaDevices
-  .getUserMedia({
-    audio: false,
-    video: {
-      facingMode: { exact: facing },
-    },
-  })
-  .then((stream) => {
-    streamRef.current = stream;
-    videoRef.current.srcObject = streamRef.current;
-    videoRef.current.onloadedmetadata = () => {
-      videoRef.current.play();
-      videoRef.current.style.height = `${videoRef.current.videoHeight}px`;
-      videoRef.current.style.width = `${videoRef.current.videoWidth}px`;
-    };
-  })
-  .catch((err) => {
-    setError(err.name);
-  });
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: { exact: facing },
+        },
+      })
+      .then((stream) => {
+        streamRef.current = stream;
+        videoRef.current.srcObject = streamRef.current;
+        videoRef.current.onloadedmetadata = () => videoRef.current.play();
+        stopQRCodeScanner(); // Останавливаем сканер перед началом нового потока
+      })
+      .catch((err) => {
+        setError(err.name);
+      });
   };
 
   const stopStream = () => {
@@ -62,11 +59,9 @@ function App() {
         .drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
     }
   
-    // Сохранение фото в data
     const data = canvasRef.current.toDataURL("image/png");
-    console.log(data); // здесь вы можете использовать data как вам угодно
+    console.log(data);
     deletePhoto();
-    // Переключение камеры
     setFacing(facing === "user" ? "environment" : "user");
   };
 
@@ -84,17 +79,7 @@ function App() {
 
   useEffect(() => {
     const config = { fps: 10, qrbox: { width: 200, height: 200 } };
-
     const html5QrCode = new Html5Qrcode("qrCodeContainer");
-
-    const qrScanerStop = () => {
-      if (html5QrCode && html5QrCode.isScanning) {
-        html5QrCode
-          .stop()
-          .then((ignore) => console.log("Scaner stop"))
-          .catch((err) => console.log("Scaner error"));
-      }
-    };
 
     const qrCodeSuccess = (decodedText) => {
       setQrMessage(decodedText);
@@ -104,35 +89,37 @@ function App() {
     if (isEnabled) {
       html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccess);
       setQrMessage("");
-      setQrCodeScanner(html5QrCode); // Установка qrCodeScanner после запуска сканера
+      setQrCodeScanner(html5QrCode);
     } else {
-      qrScanerStop();
+      stopQRCodeScanner();
     }
 
     return () => {
-      qrScanerStop();
+      stopQRCodeScanner();
     };
   }, [isEnabled]);
 
   return (
     <>
       <video
-  className={`camera-video ${facing === "user" ? "mirror" : ""}`}
-  playsInline
-  muted
-  autoPlay
-  ref={videoRef}
-></video>
-      <canvas id="qrCodeContainer" ref={canvasRef} ></canvas> 
+        className={`camera-video ${facing === "user" ? "mirror" : ""}`}
+        playsInline
+        muted
+        autoPlay
+        ref={videoRef}
+      ></video>
+      <canvas id="qrCodeContainer" ref={canvasRef}></canvas>
       {error && <div className="error">{error}</div>}
       <h3>{facing === "user" ? "FRONT CAM" : "BACK CAM"}</h3>
       <div className="controls">
         {facing === "user" && <button onClick={() => makePhoto()}><PhotoIcon /></button>}
       </div>
-      {facing === "environment" && <div className="scaner">
-        <div id="qrCodeContainer" />
-        {qrMessage && <div className="qr-message">Вы успешно прошли отметку</div>}
-      </div>}
+      {facing === "environment" && (
+        <div className="scanner-container">
+          <div id="qrCodeContainer" className="scanner"></div>
+          {qrMessage && <div className="qr-message">Вы успешно прошли отметку</div>}
+        </div>
+      )}
     </>
   );
 }
