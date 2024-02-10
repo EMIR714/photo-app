762083840351dbccc9e44a8e127373cd3b4ecb03
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-02-10 20:18:41 +0600

    fix scaner 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import React, { useRef, useEffect, useState } from "react";
-import { ReactComponent as PhotoIcon } from "./photoIcon.svg";
 import { Html5Qrcode } from "html5-qrcode";
+import { ReactComponent as PhotoIcon } from "./photoIcon.svg";
+
 import "./App.css";
 
 function App() {
-  const [error, setError] = useState();
+  const [photoData, setPhotoData] = useState(null);
   const [facing, setFacing] = useState("user");
+  const [isFirstAppEnabled, setFirstAppEnabled] = useState(true);
   const videoRef = useRef(null);
   const streamRef = useRef(null);
   const canvasRef = useRef(null);
-  const [isEnabled, setEnabled] = useState(true);
+  const html5QrCodeRef = useRef(null);
+  const [error, setError] = useState(null);
   const [qrMessage, setQrMessage] = useState("");
-  const [qrCodeScanner, setQrCodeScanner] = useState(null);
+
+  const qrCodeSuccess = (decodedText) => {
+    setQrMessage(decodedText);
+    setFirstAppEnabled(false);
+  };
 
   const startStream = () => {
     navigator.mediaDevices
-      .getUserMedia({
-        audio: false,
-        video: {
-          facingMode: { exact: facing },
-        },
-      })
+      .getUserMedia({ audio: false, video: { facingMode: { exact: facing } } })
       .then((stream) => {
         streamRef.current = stream;
         videoRef.current.srcObject = streamRef.current;
         videoRef.current.onloadedmetadata = () => videoRef.current.play();
-        if (facing === "user") {
-          stopQRCodeScanner(); // Остановить сканер при переключении на переднюю камеру
-        } else {
-          startQRCodeScanner(); // Запустить сканер при переключении на заднюю камеру
-        }
       })
       .catch((err) => {
         setError(err.name);
@@ -42,11 +39,18 @@ function App() {
     }
   };
 
-  const deletePhoto = () => {
-    const context = canvasRef.current.getContext("2d");
-    context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    context.clearRect(0, 0, -canvasRef.current.width, canvasRef.current.height);
-  };
+  // Добавляем вызов startStream при монтировании компонента
+  useEffect(() => {
+    startStream();
+  }, []);
+
+  useEffect(() => {
+    setError(null);
+    if (!isFirstAppEnabled) {
+      stopStream();
+      startStream();
+    }
+  }, [isFirstAppEnabled, facing]);
 
   const makePhoto = () => {
     const videoWidth = videoRef.current.scrollWidth;
@@ -64,72 +68,49 @@ function App() {
     }
 
     const data = canvasRef.current.toDataURL("image/png");
-    console.log(data);
-    deletePhoto();
-    setFacing(facing === "user" ? "environment" : "user");
-  };
+    setPhotoData(data);
+    setFirstAppEnabled(false);
 
-  const startQRCodeScanner = () => {
-    stopStream(); // Остановить видеопоток
-    const config = { fps: 10, qrbox: { width: 200, height: 200 } };
-    const html5QrCode = new Html5Qrcode("qrCodeContainer");
-  
-    const qrCodeSuccess = (decodedText) => {
-      setQrMessage(decodedText);
-      setEnabled(false);
-    };
-  
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccess);
-    setQrMessage("");
-    setQrCodeScanner(html5QrCode);
+    // Добавляем небольшую задержку перед запуском сканера QR-кода, чтобы дать React время на перерисовку
+    setTimeout(() => {
+      const config = { fps: 10, qrbox: { width: 200, height: 200 } };
+      const html5QrCode = new Html5Qrcode("qrCodeContainer");
+      html5QrCodeRef.current = html5QrCode;
+      html5QrCodeRef.current.start({ facingMode: "environment" }, config, qrCodeSuccess);
+    }, 0);
   };
-  
-  const stopQRCodeScanner = () => {
-    if (qrCodeScanner) {
-      qrCodeScanner.stop().catch((err) => console.error(err));
-    }
-    startStream(); // Возобновить видеопоток
-  };
-  
-  useEffect(() => {
-    setError(null);
-    stopStream();
-    startStream();
-  }, [facing]);
-
-  useEffect(() => {
-    if (!isEnabled && facing === "environment") {
-      startQRCodeScanner(); // Перезапускаем сканер, если он отключен и используется задняя камера
-    }
-  }, [isEnabled]);
 
   return (
-    <>
-      <video
-        className={`camera-video ${facing === "user" ? "mirror" : ""}`}
-        playsInline
-        muted
-        autoPlay
-        ref={videoRef}
-      ></video>
-      <canvas id="qrCodeContainer" ref={canvasRef}></canvas>
-      {error && <div className="error">{error}</div>}
-      <h3>{facing === "user" ? "FRONT CAM" : "BACK CAM"}</h3>
-      <div className="controls">
-        {facing === "user" && (
-          <button onClick={() => makePhoto()}>
-            <PhotoIcon />
-          </button>
-        )}
-      </div>
-      {facing === "environment" && (
-        <div className="scanner">
-          {qrMessage && (
-            <div className="qr-message">Вы успешно прошли отметку</div>
-          )}
+    <div className="combined-app">
+      {isFirstAppEnabled ? (
+        <>
+          <video
+            id="videoContainer"
+            className={`camera-video ${facing === "user" ? "mirror" : ""}`}
+            playsInline
+            muted
+            autoPlay
+            ref={videoRef}
+          ></video>
+          <canvas ref={canvasRef}></canvas>
+          {error && <div className="error">{error}</div>}
+          <h3>{facing === "user" ? "FRONT CAM" : "BACK CAM"}</h3>
+          <div className="controls">
+            {facing === "user" && (
+              <button onClick={makePhoto}>
+                <PhotoIcon />
+              </button>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="scaner">
+          <div id="qrCodeContainer">
+          {qrMessage && <div className="qr-message">Вы успешно прошли отметку</div>}
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
